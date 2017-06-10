bbefce9379240a9b7377b7418eef5f6bd84f7a08
devarrns <devarrns@gmail.com>
2017-06-10 21:38:29 +0900

    frontend

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
 	var json = {};
-    res.render('index.html', json);
+    res.render('main.html', json);
 });
 
 module.exports = router;
